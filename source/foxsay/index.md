b9085ba8fa314a8815e2285935a73b1afe9ff7a6
SouthFox <master@southfox.me>
2022-08-02 07:36:34 +0800

    feat: Enable foxsay

--- source/foxsay/index.md
+++ source/foxsay/index.md
@@ -3,21 +3,11 @@ title: 狐说
 date: 2021-07-11 17:45:57
 ---
 
-<html>
 <link rel="stylesheet" href="/css/foxsay.css" media="screen" type="text/css">
-    <main id="app">
-        <aside>
-        </aside>
-        <section class="item" v-for="item in contents" v-cloak>
-            <bb v-html='item.attributes.content'></bb>
-            <time v-html:datetime="item.attributes.time">{{item.attributes.time}}</time>
-        </section>
-        <div class="load-ctn">
-            <button class="btn btn-default " v-on:click="loadMore" v-if="contents" v-cloak>加载更多</button>
-            <p class="tip" v-else>加载中……</p>
-        </div>
-    </main>
-<script src="https://cdn.bootcss.com/vue/2.6.11/vue.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/leancloud-storage@4.5.3/dist/av-min.js"></script>
+<main id='app'>
+    <div id="foxsay">
+    </div>
+<center><a target="_blank" href="https://foxsay.southfox.me/@SouthFox" type="button" class="btn btn-default"><i class="fa fa-paw"></i>到原始站点查看</a></center>
+</main>
+<script src="../js/rss-parser.min.js"></script>
 <script src="../js/foxsay.js"></script>
-</html>
--- themes/freemind/source/js/foxsay.js
+++ themes/freemind/source/js/foxsay.js
@@ -1,60 +1,19 @@
-    var {
-        Query
-    } = AV;
-    AV.init({
-        appId: "hINvYWepO1gnhcNIPEycw7o5-MdYXbMMI", //你的 leancloud 应用 id （设置-应用keys-AppID）
-        appKey: "9PFMoeAb7yW62fAyNYdVDHAQ", //你的 leancloud 应用 AppKey （设置-应用keys-AppKey）
-    });
+let parser = new RSSParser();
+parser.parseURL('https://foxsay.southfox.me/@SouthFox.rss', function(err, feed) {
+  if (err) throw err;
+  for (let i = 0; i < 10; i++) {
+    const entry = feed.items[i];
+    printPost(entry);
+  }
+});
 
-    var query = new AV.Query('content');
-    
-    var app = new Vue({
-        el: '#app',
-        data: {
-            page: 0,
-            count: 0,
-            contents: []
-        },
-        methods: {
-            loadMore: function(event) {
-                getData(++this.page);
-            }
-        }
-    })
-    
-    function urlToLink(str) {
-        var re = /(http|ftp|https):\/\/[\w-]+(.[\w-]+)+([\w-.,@?^=%&:/~+#]*[\w-\@?^=%&/~+#])?/g;;
-    
-        str = str.replace(re, function(website) {
-            if (str.indexOf("cf-ipfs") != -1 ) {
-                return "<img src='" + website + "' alt='图片'></img>"
-            } else {
-            return "<a href='" + website + "' target='_blank'> <i class='iconfont icon-lianjie-copy'></i>链接 </a>";
-            }
-        });
-        return str;
-    }
-    
-    function getData(page = 0) {
-        query.descending('createdAt').skip(page * 20).limit(20).find().then(function(results) {
-            if (results.length == 0) {
-                alert('无了。')
-            } else {
-                let resC = results;
-                reqData = false;
-                resC.forEach((i) => {
-                    let dateTmp = new Date(i.createdAt);
-                    i.attributes.content = "<p>" + urlToLink(i.attributes.content) + "</p>";
-                    i.attributes.time = `${dateTmp.getFullYear()}-${(dateTmp.getMonth() + 1) < 10 ? ('0' + (dateTmp.getMonth()+1)) : dateTmp.getMonth()+1}-${(dateTmp.getDate() + 1) < 10 ? ('0' + dateTmp.getDate()) : dateTmp.getDate()} ${(dateTmp.getHours() + 1) <= 10 ? ('0' + dateTmp.getHours()) : dateTmp.getHours()}:${(dateTmp.getMinutes() + 1) <= 10 ? ('0' + dateTmp.getMinutes()) : dateTmp.getMinutes()}`;
-                    app.contents.push(i);
-                })
-            }
-    
-        }, function(error) {});
-    }
-    
-    getData(0);
-    
-    query.count().then(function(count) {
-        app.count = count;
-    }, function(error) {});
+function printPost(entry) {
+  section = document.createElement("section");
+  section.classList.add('item');
+  popContent = entry.contentSnippet;
+
+  section.innerHTML = popContent;
+
+  blogs = document.getElementById('foxsay');
+  blogs.append(section);
+}
