ae5e71cdd3b44f047788da702951068be9e1349a
SouthFox <master@southfox.me>
2023-02-03 18:49:57 +0800

    site/change comment service

--- utils.py
+++ utils.py
@@ -159,7 +159,7 @@ if __name__ == '__main__':
             filelist = []
             postlist = []
             fontChar()
-            addDiscussion()
+            # addDiscussion()
         elif opt in ('-b', '--backupimg'):
             filelist = []
 
