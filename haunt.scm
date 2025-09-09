29e6670e93608e76353e2659b3e6715af38c011b
SouthFox <master@southfox.me>
2025-09-09 23:55:28 +0800

    [feat] add hexo-post-slug

--- haunt.scm
+++ haunt.scm
@@ -5,7 +5,24 @@
              (haunt builder assets)
              (haunt reader commonmark)
              (haunt site)
+             (ice-9 string-fun)
              (hole theme))
+
+
+(define (hexo-post-slug post)
+  (or (post-ref post 'slug)
+      (let* ((%post-list (cdr (string-split (post-file-name post) #\/)))
+             (post-list (if (equal? "_posts" (car %post-list))
+                            (cdr %post-list)
+                            %post-list))
+             (list-length (length post-list)))
+        (string-join (append (list-head post-list (- list-length 1))
+                             (list (string-replace-substring
+                                    (list-ref post-list (- list-length 1))
+                                    ".md"
+                                    "/index"
+                                    )))
+                     "/"))))
 
 (site #:title "狐狸反走矣"
       #:domain "blog.southfox.me"
@@ -20,4 +37,5 @@
                         )
                        (atom-feed)
                        (static-directory "assets")
-                       ))
+                       )
+      #:make-slug hexo-post-slug)
