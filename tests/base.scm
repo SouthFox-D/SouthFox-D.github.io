75d3b40ba9ba0421347e6124ae39b282d07f3de3
SouthFox <master@southfox.me>
2026-02-25 17:38:53 +0800

    [test] add list tests

--- tests/base.scm
+++ tests/base.scm
@@ -14,5 +14,21 @@
 
 (test-equal (hole/org->sxml "* H1") '((h1 (@ (id "H1")) "H1")))
 
+(test-equal
+    (hole/org->sxml "- 1\n- 2\n- 3")
+  '((ul (li "1") (li "2") (li "3"))))
+
+(test-equal
+    (hole/org->sxml "- 1\n- 2\n-3")
+  '((ul (li "1") (li "2" "\n" "-3"))))
+
+(test-equal
+    (hole/org->sxml "1. 1\n2. 2\n3. 3")
+  '((ol (li "1") (li "2") (li "3"))))
+
+(test-equal
+    (hole/org->sxml "1. 1\n2. 2\n3.3")
+  '((ol (li "1") (li "2" "\n" "3.3"))))
+
 
 (test-end "sxml-test")
